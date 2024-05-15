6381db95fe26b996e69fa378e995e794b8f95bc3
LOUISENZR <louisanaza@gmail.com>
2024-05-15 08:48:29 +0700

    code

---
+++ SLLMain.java
@@ -0,0 +1,14 @@
+public class SLLMain {
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList();
+        myLinkedList.print();
+        myLinkedList.addFirst(900);
+        myLinkedList.print();
+        myLinkedList.addFirst(700);
+        myLinkedList.print();
+        myLinkedList.addLast(500);
+        myLinkedList.print();
+        myLinkedList.insertAfter(700, 300);
+        myLinkedList.print();
+    }
+}
